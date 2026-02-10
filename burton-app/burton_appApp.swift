2855eca990fb2f6ef93afa2cdf25319a88a2951b
b32griff <b32griff@gmail.com>
2026-02-10 13:21:24 -0700

    Initial Commit

---
+++ burton-app/burton_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  burton_appApp.swift
+//  burton-app
+//
+//  Created by Burton  Griffin on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct burton_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
